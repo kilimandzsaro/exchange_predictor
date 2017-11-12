5ee9e443f132294d27adc48ceb185dcf8c0b2642
kilimandzsaro <natalia.bakos@inbank.ee>
2017-11-12 18:06:12 +0200

    Add validations and tests for supported_currency model

--- app/models/supported_currency.rb
+++ app/models/supported_currency.rb
@@ -1,5 +1,8 @@
 # Supported currencies, only the application user
 # can extend this list (or a DB admin)
 class SupportedCurrency < ApplicationRecord
-  validates :currency_code, uniqueness: true
+  validates :currency_code, uniqueness: true, format: {
+    with: /[A-Z]{3}/,
+    message: "Only valid currency codes allowed"
+  }
 end
--- spec/models/supported_currency_spec.rb
+++ spec/models/supported_currency_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe SupportedCurrency, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is filled with valid currency format" do
+    expect(SupportedCurrency.new(currency_code: "XXX")).to be_valid
+  end
+  it "is n ot accepting a not valid currency format" do
+    currencies = ["xxx", "something", "AAc", "111", ""]
+    currencies.each do |currency|
+      c = SupportedCurrency.new(currency_code: currency)
+      expect(c).to_not be_valid
+    end
+  end
 end
